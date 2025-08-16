7b6755abf5cc12a73d93bdbe8095ab129be58bf2
Anurag20075 <goyalanurag2005@gmail.com>
2025-08-16 22:50:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Component/NavBar";
-// import GamerHub from "./Component/HomePage";
 import Home from "./Component/Home";
 function App() {
   return (
