515cc215401736ab61a2f2787c0d4a7fea121eba
Anurag20075 <goyalanurag2005@gmail.com>
2025-08-16 22:55:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./Component/NavBar";
-import Home from "./Component/Home";
+// import Home from "./Component/Home";
+import Categories from "./Component/CategoryPAge";
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
